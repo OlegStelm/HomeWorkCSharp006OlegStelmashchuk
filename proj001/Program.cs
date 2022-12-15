70160d6132b0e9245827441f8941c4ca92dcb398
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-16 00:49:18 +0300

    EditProj001

--- proj001/Program.cs
+++ proj001/Program.cs
@@ -3,15 +3,16 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3 // ошибка, вместо 3 -> 4    */
 
-int[] array = new int[6];
+int m;
+Console.WriteLine("Enter the number of elements: ");
+int.TryParse(Console.ReadLine()!, out m);
+int[] array = new int[m];
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    int a;
     Console.WriteLine("Enter a number: ");
-    int.TryParse(Console.ReadLine()!, out a);
-    array[i] = a;
-     if (array[i]>0)
+    int.TryParse(Console.ReadLine()!, out array[i]);
+    if (array[i]>0)
     {
         count++;
     }
