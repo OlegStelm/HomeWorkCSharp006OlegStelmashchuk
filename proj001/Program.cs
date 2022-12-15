78a30d5af7c6605ce368081001255efaf67b957a
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-15 22:43:48 +0300

    InitCommit

---
+++ proj001/Program.cs
@@ -0,0 +1,19 @@
+﻿/*  **Задача 41**
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 // ошибка, вместо 3 -> 4    */
+
+int[] array = new int[6];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    int a;
+    Console.WriteLine("Enter a number: ");
+    int.TryParse(Console.ReadLine()!, out a);
+    array[i] = a;
+     if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.Write($"Number of numbers greater than '0' -> {count}");
